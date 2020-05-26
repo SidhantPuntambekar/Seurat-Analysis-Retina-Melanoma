62793e766a5fa6443b93f5cbe4a949251bf2dc80
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2020-05-26 09:49:15 -0600

    Added code for AnalysisMelanoma.r

---
+++ AnalysisMelanoma.r
@@ -0,0 +1,22 @@
+#library(deplyr)
+#library(Seurat)
+#library(patchwork)
+remotes::install_github("rnabioco/clustifyr")
+library(clustifyr)
+library(tidyverse)
+library(usethis)
+
+#humanMelanomaDC <- read_tsv(~/scRNA-seq-Practice/data_Spleen_Melanoma/melanoma/human_melanoma_cell_metadata_9315x14.tsv) %>%
+  #select(-X1) %>% t()
+mat_humanMelanomaDC <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE137nnn/GSE137710/suppl/GSE137710_human_melanoma_counts_normalized_9315x19445.tsv.gz") %>%
+  select(-X1) %>% t()
+colnames(mat_humanMelanomaDC) <- seq_len(ncol(mat_humanMelanomaDC))
+meta_humanMelanomaDC <- read_tsv("ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE137nnn/GSE137710/suppl/GSE137710_human_melanoma_cell_metadata_9315x14.tsv.gz") %>%
+  mutate("rn" = row_number())
+ref_humanMelanomaDC <- average_clusters(
+  mat_humanMelanomaDC,
+  meta_humanMelanomaDC,
+  "cell_type"
+)
+
+usethis::use_data(ref_humanMelanomaDC, compress = "xz", overwrite = TRUE)
