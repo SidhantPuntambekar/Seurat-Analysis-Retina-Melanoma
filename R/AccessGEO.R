f0100ba890ce225f6ede27a7abd097187a0523fd
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2020-06-02 13:43:45 -0600

    Delete erroneous Rebol file

--- R/AccessGEO.R
+++
@@ -1,6 +0,0 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("GEOquery")
-GEOquery::getGEOSuppFiles("GSM1514028", fetch_files = F)
-GEOquery::getGEOSuppFiles("GSE137710", fetch_files = F)
