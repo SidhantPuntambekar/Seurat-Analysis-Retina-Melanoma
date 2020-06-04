2d009b6c86472ff51884f77d96a7b23c0c15f22c
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2020-06-04 14:40:22 -0600

    Gather data from GEO using Geoquery

---
+++ R/ReferenceMatrixDataGathering.r
@@ -0,0 +1,9 @@
+#Query for GEO ref matrix
+#"expression profiling by high throughput sequencing"[DataSet Type] AND ("mtx"[Supplementary Files] OR "tsv"[Supplementary Files] OR "csv"[Supplementary Files] OR "txt"[Supplementary Files]) AND "single cell"[All Fields] AND "metadata"[All Fields]
+records <- read.csv(file = "~/scRNA-seq-Practice/data_Records/records.csv")
+head(records)
+for (i in 1:5)
+{
+  print(GEOquery::getGEOSuppFiles(records[i,1,1], fetch_files = F))
+  print(i)
+}
