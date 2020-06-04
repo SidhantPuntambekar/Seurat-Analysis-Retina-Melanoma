8e389914fe72413b4705baa66288fc39884923ab
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2020-06-04 08:50:23 -0600

    Added scripts to generate GSM, GSE, GDS objects

--- R/AccessGEO.r
+++ R/AccessGEO.r
@@ -5,3 +5,19 @@ BiocManager::install("GEOquery")
 GEOquery::getGEOSuppFiles("GSM1514028", fetch_files = F)
 GEOquery::getGEOSuppFiles("GSE137710", fetch_files = F)
 GEOquery::getGEOSuppFiles("GSE139829", fetch_files = F)
+browseVignettes("GEOquery")
+
+library(GEOquery)
+gds <- getGEO("GDS507")
+gsm <- getGEO("GSM11805")
+head(Meta(gsm))
+
+Table(gsm)[1:5,]
+Columns(gsm)
+Columns(gds)[,1:3]
+
+gse <- getGEO("GSE781", GSEMatrix = FALSE)
+head(Meta(gse))
+names(GSMList(gse))
+GSMList(gse)[[1]]
+names(GPLList(gse))
