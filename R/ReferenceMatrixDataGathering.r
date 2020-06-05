3a5bdfd338e68ba5488a63adcc0ba9f7f19a3566
Sidhant Puntambekar <35539945+SidhantPuntambekar@users.noreply.github.com>
2020-06-05 10:10:38 -0600

    Changed file path and printed Geoquery

--- R/ReferenceMatrixDataGathering.r
+++ R/ReferenceMatrixDataGathering.r
@@ -1,8 +1,9 @@
 #Query for GEO ref matrix
 #"expression profiling by high throughput sequencing"[DataSet Type] AND ("mtx"[Supplementary Files] OR "tsv"[Supplementary Files] OR "csv"[Supplementary Files] OR "txt"[Supplementary Files]) AND "single cell"[All Fields] AND "metadata"[All Fields]
-records <- read.csv(file = "~/scRNA-seq-Practice/data_Records/records.csv")
+records <- read.csv(file = "~/scRNA-seq-Practice/data_Reference_Matrix/records.csv")
 head(records)
-for (i in 1:5)
+tail(records)
+for (i in 1:36)
 {
   print(GEOquery::getGEOSuppFiles(records[i,1,1], fetch_files = F))
   print(i)
